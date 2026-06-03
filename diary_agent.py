5c29e64c4192b128461dcf72a8d43f33e2e2a068
0anant <bigbluemagic06@gmail.com>
2026-06-03 12:35:12 +0530

    simple diary_agent

---
+++ diary_agent.py
@@ -0,0 +1,19 @@
+def write_diary_tool(entry):
+    with open("dairy.txt", "w") as file:
+        file.write(entry)
+    print("Diary is updated")
+
+def read_tool():
+    with open("dairy.txt", "r") as file:
+        words = file.read()
+        return words
+    
+print("Diary Agent is running....")
+print("Let me read read what I wrote...")
+
+write_diary_tool("I love learning new things related to tech, specially related to AI.")
+
+memory = read_tool()
+
+print("My Dairy says", memory)
+print("My Diary Agent is sleeping...")
